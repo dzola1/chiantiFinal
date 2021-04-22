444c8deecea7e166ac2efa6de61b5800bf8e6445
shannonmiller211 <55462041+shannonmiller211@users.noreply.github.com>
2021-04-22 12:20:59 -0400

    admin page

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Employee, Date, Availability
+
+admin.site.register(Availability)
+admin.site.register(Employee)
+admin.site.register(Date)
+
+
